7c8372ee366b80faf813c92c33d7a13f6c426454
mohsen <131223921+muhamedmuhsen@users.noreply.github.com>
2026-01-23 20:38:43 +0200

    feat(search): include alternative names in search filtering
    - Update `filterCitiesAndDistricts` to check `cityOtherName` and `districtOtherName` fields.
    - Improve search coverage by allowing users to find locations using their alternative naming conventions.
    - Clean up imports and formatting in `SearchViewModel`.

--- app/src/main/java/com/example/bostaassessment/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/example/bostaassessment/presentation/search/SearchViewModel.kt
@@ -3,14 +3,17 @@ package com.example.bostaassessment.presentation.search
 import com.example.bostaassessment.domain.util.Result
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bostaassessment.domain.usecase.SearchUseCase
 import com.example.bostaassessment.presentation.utils.state.UiState
 import com.example.bostaassessment.presentation.utils.strings.asUiText
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,6 +25,7 @@ class SearchViewModel @Inject constructor(private val searchUseCase: SearchUseCa
     init {
         loadCitiesAndDistricts()
     }
+
     fun onSearchQueryChange(searchQuery: String) {
         _uiState.update { _uiState.value.copy(searchQuery = searchQuery) }
         filterCitiesAndDistricts(_uiState.value.searchQuery)
@@ -31,13 +35,15 @@ class SearchViewModel @Inject constructor(private val searchUseCase: SearchUseCa
         _uiState.update { state ->
             state.copy(
                 filteredCities = state.cities.filter { city ->
-                    city.cityName.contains(searchQuery, ignoreCase = true) ||
-
-                            city.districts.any {
-                                it.districtName.contains(
-                                    searchQuery, ignoreCase = true
-                                )
-                            }
+                    city.cityName.contains(
+                        searchQuery,
+                        ignoreCase = true
+                    ) || city.cityOtherName.contains(searchQuery) || city.districts.any {
+                        it.districtName.contains(
+                            searchQuery,
+                            ignoreCase = true
+                        ) || it.districtOtherName.contains(searchQuery)
+                    }
                 })
         }
     }
